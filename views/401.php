c3164409227d4dd8d8d341ac3487f025365604cf
Reyna <al060872@uacam.mx>
2021-11-17 13:02:42 -0600

    Se añadieron estilos para el codigo 401 :no_entry_sign: :pound:

---
+++ views/401.php
@@ -0,0 +1,14 @@
+<link rel="stylesheet" href="css/style_401.css">
+
+<body>
+        <h1>401 Error Page #1</h1>
+        <p class="zoom-area"><b>CSS</b> animations to make a cool 401 page. </p>
+        <section class="error-container">
+            <span><span>4</span></span>
+            <span>0</span>
+            <span><span>1</span></span>
+        </section>
+        <div class="link-container">
+            <a target="_blank" href="index.jsp" class="more-link">Visit the original article</a>
+        </div>
+    </body>
